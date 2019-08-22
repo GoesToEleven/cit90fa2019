5fabbd0a8459baaf18ac408532b9674609b144d6
Todd McLeod <tuddleymc@gmail.com>
2019-08-21 18:57:42 -0700

    adds hello world first file

---
+++ 001-hello-world/hello.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello")
+
+	// const name, age = "Kim", 22
+	// fmt.Println(name, "is", age, "years old.")
+}
+
+// control flow
+
+/*
+ SEQUENTIALLY (top to bottom)
+
+ CONDITIONALLY (if statements)
+
+ ITERATIVELY (loop)
+
+*/
