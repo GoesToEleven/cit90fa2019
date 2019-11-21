90cffb8494ab6d92e3382b27da95a8b08c6fd2bd
Todd McLeod <tuddleymc@gmail.com>
2019-11-20 20:10:53 -0800

    yeah

---
+++ 87-steps/03-image/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
+func main() {
+	http.Handle("/resources/", http.StripPrefix("/resources", http.FileServer(http.Dir("./assets"))))
+	http.HandleFunc("/", foo)
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
